2621918a0a97d5d9582d94576af22def01d71729
Faris Chaudhry <faris.chaudhry@outlook.com>
2024-08-27 21:41:42 +0100

    AgentRun struct in AlphaEnsemble.py to manage LLM calls

--- contracts/contracts/AlphaEnsemble.sol
+++ contracts/contracts/AlphaEnsemble.sol
@@ -6,28 +6,40 @@ import "./interfaces/IOracle.sol";
 contract AlphaEnsemble {
 
     struct Agent {
-        // Profit and Loss
-        int256 pnl;
-        // Position of each asset (TICKER => POSITION)
-        mapping(string => uint256) positions;
+        int256 pnl; // Profit and Loss
+        mapping(string => uint256) positions; // Position of each asset (TICKER => POSITION)
+    }
+
+    struct AgentRun {
+        IOracle.Message[] messages;
+        uint responsesCount;
+        uint8 max_iterations;
+        bool is_finished;
     }
 
     Agent[] public agents;
-    uint256 public lastUpdateTime;
+    mapping(uint => AgentRun) public agentRuns;
+    uint public agentRunCount; // Counter for the number of agent runs
+    uint256 public lastUpdateTime; // Timestamp of last update
 
     address public oracleAddress;
 
-    event PositionsUpdated(uint indexed agentID, string[] assets, int256[] positions)
+    event PositionsUpdated(uint indexed agentID, string[] assets, int256[] positions);
+    event AgentRunCreated(uint indexed agentId, uint indexed runId);
 
-    // Constructor
     constructor(address _oracleAddress, uint256 numAgents) {
-        oracleAddress = _oracleAddress;  // Set the oracle address during contract deployment
-        initializeAgents(numAgents);  // Initialize the fixed number of agents
+        oracleAddress = _oracleAddress; // Set the oracle address during contract deployment
+        initializeAgents(numAgents); // Initialize the fixed number of agents
     }
 
+    /**
+     * @notice Initialize the fixed number of agents
+     * @param numAgents Number of agents to initialize
+     */
     function initializeAgents(uint256 numAgents) private {
+        // Add an agent to the agents array
         for (uint256 i = 0; i < numAgents; i++) {
-            agents.push(); // Add an agent to the agents array
+            agents.push();
         }
     }
 }
