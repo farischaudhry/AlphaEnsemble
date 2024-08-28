707aa9f404c788b889e40f8cfa67f713c6a8be68
Faris Chaudhry <faris.chaudhry@outlook.com>
2024-08-28 10:15:12 +0100

    Add Header, Footer and their respective styles

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="App-footer">
+      <p>&copy; Made by Faris Chaudhry, Sriharsha Vitta, Siddhant Gadkari.</p>
+    </footer>
+  );
+}
+
+export default Footer;
