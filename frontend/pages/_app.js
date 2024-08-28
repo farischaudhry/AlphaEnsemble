cc85e23ef3e773e8fdea64732a334cb71f6bfeae
Faris Chaudhry <faris.chaudhry@outlook.com>
2024-08-28 12:41:39 +0100

    switch to next.js for server side rendering

---
+++ frontend/pages/_app.js
@@ -0,0 +1,10 @@
+import '../styles/App.css';
+import '../styles/index.css';
+import '../styles/Footer.css';
+import '../styles/Header.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+export default function Home() {
+  return (
+    <div className="App">
+      <Header />
+      <main>
+        <h1>Hello World</h1>
+      </main>
+      <Footer />
+    </div>
+  );
+}
