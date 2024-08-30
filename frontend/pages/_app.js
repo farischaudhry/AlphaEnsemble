de8c7155952d5dc333ab74ae207b3826606677e4
Faris Chaudhry <faris.chaudhry@outlook.com>
2024-08-30 11:46:38 +0100

    Modularise styles with module.css

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,10 @@
 import '../styles/globals.css';
+import '../styles/Header.module.css';
+import '../styles/Footer.module.css';
+import '../styles/Leaderboard.module.css';
+import '../styles/InstrumentOverview.module.css';
+import '../styles/DynamicGraph.module.css';
+import '../styles/App.module.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
